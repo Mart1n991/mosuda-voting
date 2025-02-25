d12bf7176437cf9f6c3f1e71ade479236603e93b
Martin Javorsky <martinjavorsky8@gmail.com>
2025-02-25 22:39:35 +0100

    Fix matcher for default locales

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,13 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ["/", "/(sk|en|cz)/:path*"],
+  matcher: [
+    // Match all pathnames except for
+    // - /api (API routes)
+    // - /_next (Next.js internals)
+    // - /_vercel (Vercel internals)
+    // - /static (public files)
+    // - .*\\..*$ (files with extensions)
+    "/((?!api|_next|_vercel|static|.*\\..*).*)",
+  ],
 };
