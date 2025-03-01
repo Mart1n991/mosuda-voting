cd8cab3640d1cf432274dce2ee9285dda47051f1
Martin Javorsky <martinjavorsky8@gmail.com>
2025-03-01 20:27:36 +0100

    Fix nvmrc syntax

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,21 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https" as const,
+        hostname: "firebasestorage.googleapis.com",
+      },
+    ],
+  },
+  modularizeImports: {
+    // Transform `import { map } from 'lodash'` to `import map from 'lodash/map'`
+    lodash: {
+      transform: "lodash/{{member}}",
+    },
+  },
+};
 
 export default withNextIntl(nextConfig);
