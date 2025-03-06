6ab6a69185301bbaebe81ef38dac07d79c00db87
Martin Javorsky <martinjavorsky8@gmail.com>
2025-03-06 23:03:07 +0100

    Fix params types

--- src/app/[locale]/coach-detail/[id]/page.tsx
+++ src/app/[locale]/coach-detail/[id]/page.tsx
@@ -1,8 +1,9 @@
 import { getCoachDetail } from "@/services/getCoachDetail";
 import { CoachDetail } from "./CoachDetail";
 
-export default async function CoachDetailPage({ params }: { params: { id: string } }) {
-  const coachDetail = await getCoachDetail(params.id);
+export default async function CoachDetailPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  const coachDetail = await getCoachDetail(id);
 
   return (
     <main className="max-w-screen-xl xl:mx-auto my-20 mx-10 flex flex-col items-center">
