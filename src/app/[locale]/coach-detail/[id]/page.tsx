111f9efa39e77370702837f4d91dad0d0fc7d7bd
Martin Javorsky <martinjavorsky8@gmail.com>
2025-03-19 22:53:27 +0100

    Added navbar to coach detail

--- src/app/[locale]/coach-detail/[id]/page.tsx
+++ src/app/[locale]/coach-detail/[id]/page.tsx
@@ -1,13 +1,17 @@
 import { getCoachDetail } from "@/services/getCoachDetail";
 import { CoachDetail } from "./CoachDetail";
+import { Navbar } from "@/components/Navbar";
 
 export default async function CoachDetailPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   const coachDetail = await getCoachDetail(id);
 
   return (
-    <main className="max-w-screen-xl xl:mx-auto md:my-20 md:mx-10 flex flex-col items-center">
-      <CoachDetail coachDetail={coachDetail} />
-    </main>
+    <>
+      <Navbar />
+      <main className="max-w-screen-xl xl:mx-auto md:my-20 md:mx-10 flex flex-col items-center">
+        <CoachDetail coachDetail={coachDetail} />
+      </main>
+    </>
   );
 }
