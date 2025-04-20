a89c7769f782c9329d66a9d57ba999bb5e5c2a4d
Martin Javorsky <martinjavorsky8@gmail.com>
2025-04-20 21:51:17 +0200

    Polish voters design. add correct texts for voters page

--- src/app/[locale]/voter/page.tsx
+++ src/app/[locale]/voter/page.tsx
@@ -1,3 +1,4 @@
+import { Accordion } from "@/components/Accordion";
 import { Footer } from "@/components/Footer/Footer";
 import { Navbar } from "@/components/Navbar";
 import PartnersLogos from "@/components/PartnersLogos/PartnersLogos";
@@ -55,51 +56,46 @@ export default async function Voter() {
   return (
     <>
       <Navbar />
-      <main className="bg-gradient-to-br from-mosuda-green-light to-mosuda-green-dark">
+      <main className="md:bg-gradient-to-br bg-gradient-to-b from-white from-15% md:from-30% to-mosuda-green-light">
         <div className="max-w-screen-xl xl:mx-auto py-5 xl:py-20 flex flex-col items-center">
           <section className="flex flex-col xl:flex-row items-center xl:items-start w-full px-4 sm:px-10 mb-10">
             <div className="flex-1 flex flex-col items-center xl:items-start">
               <h1 className="whitespace-pre-line text-2xl md:text-5xl font-bold text-center xl:text-left">
                 {t.rich("heading", {
-                  region: (chunks) => <span className="underline decoration-white">{chunks}</span>,
+                  region: (chunks) => <span className="underline decoration-mosuda-green-light">{chunks}</span>,
                 })}
               </h1>
-              <p className="mt-2 text-stone-100">{t("date")}</p>
+              <p className="mt-2 text-stone-700 text-center xl:text-left text-sm md:text-base">{t("date")}</p>
               <p className="max-w-[500px] text-stone-700 mt-6 text-center xl:text-left text-sm md:text-base">
                 {t("description")}
               </p>
               <div className="flex gap-4 mt-10">
-                <Link href={routes.coaches} className={cn(buttonVariants({ variant: "secondary", size: "lg" }), "shadow-lg")}>
+                <Link href={routes.coaches} className={cn(buttonVariants({ size: "lg" }), "shadow-lg")}>
                   {t("vote")}
                 </Link>
               </div>
+
               <div className="mt-10 w-[80%] hidden xl:block">
-                <p className="font-bold tet-2xl mb-4">{t("howToVote")}</p>
-                <p className="text-sm text-stone-700">
-                  {t.rich("howToVoteDescription", {
-                    vote: (chunks) => (
-                      <Link href={routes.coaches} className="cursor-pointer underline decoration-white">
-                        {chunks}
-                      </Link>
-                    ),
-                  })}
-                </p>
+                <Accordion
+                  items={[
+                    { title: t("howToVote"), content: t("howToVoteDescription") },
+                    { title: t("whatIcanWin"), content: t("whatIcanWinDescription") },
+                    { title: t("howAndWhenIfIWin"), content: t("howAndWhenIfIWinDescription") },
+                  ]}
+                />
               </div>
             </div>
 
             <PrizeCarousel prizes={prizes} className="mt-10 xl:mt-0" />
 
             <div className="mt-10 block w-full max-w-[500px] xl:hidden">
-              <p className="font-bold tet-2xl text-center xl:text-left">{t("howToVote")}</p>
-              <p className="text-sm text-stone-700 text-center xl:text-left">
-                {t.rich("howToVoteDescription", {
-                  vote: (chunks) => (
-                    <Link href={routes.coaches} className="cursor-pointer underline decoration-white">
-                      {chunks}
-                    </Link>
-                  ),
-                })}
-              </p>
+              <Accordion
+                items={[
+                  { title: t("howToVote"), content: t("howToVoteDescription") },
+                  { title: t("whatIcanWin"), content: t("whatIcanWinDescription") },
+                  { title: t("howAndWhenIfIWin"), content: t("howAndWhenIfIWinDescription") },
+                ]}
+              />
             </div>
           </section>
         </div>
