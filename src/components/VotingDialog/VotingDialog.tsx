593534d835686a7328f28b193503444cebd9b109
Martin Javorsky <martinjavorsky@Mac.home>
2025-11-22 21:51:15 +0100

    Edit the Badge in Votingdialog

--- src/components/VotingDialog/VotingDialog.tsx
+++ src/components/VotingDialog/VotingDialog.tsx
@@ -1,4 +1,10 @@
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import { CoachProfile } from "@/types/CoachProfile";
 import { useTranslations } from "next-intl";
 import { VotingForm } from "../VotingForm";
@@ -11,19 +17,27 @@ type VotingDialogProps = {
   coachProfile: CoachProfile;
 };
 
-export const VotingDialog = ({ open, onOpenChange, coachProfile }: VotingDialogProps) => {
+export const VotingDialog = ({
+  open,
+  onOpenChange,
+  coachProfile,
+}: VotingDialogProps) => {
   const t = useTranslations("coachListPage");
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="p-4 md:p-10 md:max-w-[600px]">
         <DialogHeader className="items-start">
-          <p className="text-xs text-stone-500">{t("votingDialog.votingFor")}</p>
+          <p className="text-xs text-stone-500">
+            {t("votingDialog.votingFor")}
+          </p>
           <div className="flex gap-4 sm:gap-10 items-start sm:items-center justify-between sm:justify-start w-full sm:w-auto">
             <DialogTitle className="text-xl sm:text-2xl font-bold text-left flex-1">
               {truncateText(coachProfile.name, 35)}
             </DialogTitle>
-            <Badge size="lg">{t("votes", { count: coachProfile.voteCount })}</Badge>
+            <Badge size="lg" className="bg-amber-400">
+              {t("votes", { count: coachProfile.ranking })}
+            </Badge>
           </div>
           <DialogDescription />
         </DialogHeader>
